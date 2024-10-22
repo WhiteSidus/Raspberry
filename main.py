65a0c1dd0f19b4cfe082191365ec6fe16f1d080c
Whiteside <vovadxc@gmail.com>
2024-10-22 13:42:46 +0200

    comm

--- main.py
+++ main.py
@@ -6,41 +6,15 @@ led2 = machine.Pin(1, machine.Pin.OUT)
 led3 = machine.Pin(2, machine.Pin.OUT)
 led4 = machine.Pin(3, machine.Pin.OUT)
 
-a = True
+leds = [led, led2, led3, led4]
 
-while a:
-    led.value(1)
-    time.sleep(0.5)  
-    led.value(0)
-    time.sleep(0.5)
-    
-    led2.value(1)
-    time.sleep(0.5)  
-    led2.value(0)
-    time.sleep(0.5)
+def LightOption(Pin, option, delay):
+    time.sleep(delay)
+    return Pin.value(option)
 
-    led3.value(1)
-    time.sleep(0.5)  
-    led3.value(0)
-    time.sleep(0.5)
+while True:
+    for led in leds:
+        LightOption(led, 1, 1)
+    for led in leds:
+        LightOption(led, 0, 0)
 
-    led4.value(1)
-    time.sleep(0.5)  
-    led4.value(0)
-    time.sleep(0.5)
-
-    led.value(1)
-    time.sleep(0.5)
-    led2.value(1)
-    time.sleep(0.5)
-    led3.value(1)
-    time.sleep(0.5)
-    led4.value(1)
-
-    led.value(0)
-    time.sleep(0.5)
-    led2.value(0)
-    time.sleep(0.5)
-    led3.value(0)
-    time.sleep(0.5)
-    led4.value(0)
