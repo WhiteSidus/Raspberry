4bfd74e1845330089b2b4ff81c8d80303dc1f5a1
Whiteside <vovadxc@gmail.com>
2024-11-19 18:45:53 +0100

    3

--- text.py
+++ text.py
@@ -10,29 +10,30 @@ led3 = machine.Pin(4, machine.Pin.OUT)
 led4 = machine.Pin(5, machine.Pin.OUT)
 
 button = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_UP)
-show_temperature = True  
+show_temperature = True  # Výchozí režim
 
 while True:
     if button.value() == 0:
-        show_temperature = not show_temperature
-        time.sleep(0.2)
+        show_temperature = False
+        time.sleep(0.02)
 
     sensor.measure()  
     temperature = sensor.temperature()
 
     if show_temperature:
-        print("Temperature:", temperature, ".C")
+        print("Temperature:", temperature, "°C")
     else:
         print("Humidity:", sensor.humidity(), "%")
-
 
     led1.value(temperature < 20)
     led2.value(20 <= temperature < 25)
     led3.value(25 <= temperature < 30)
     led4.value(temperature >= 30)
     
+    # Aktivace buzzera na krátkou dobu
     buzzer.on()
-    time.sleep(0.003)
+    time.sleep(0.01)
     buzzer.off()
 
-    time.sleep(1)
+    # Pauza mezi iteracemi
+    time.sleep(0.3)
